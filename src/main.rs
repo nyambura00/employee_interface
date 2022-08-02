9daedf8f46045f88204cf75014ee922ba43afa50
nyambura00 <winnienyambura14@gmail.com>
2022-08-02 11:25:31 +0300

    optimize approach

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,55 @@
 //Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company.
 //For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
 use std::collections::HashMap;
-use std::{io, string};
+use std::io;
 
-struct Employee{
+/*struct Employee{
     name: String,
     department: String,
-}
+}*/
 fn main() {
-    let mut employees = Vec::new();
-    let mut full_house:HashMap<&str, &str> = HashMap::new();
+    //hashmap of employee name, department
+    let mut employees = HashMap::new();
     loop {
-        println!("Enter a command: add_member, list_members, delete_member");
-        let mut command: String = String::new();
+        println!("Enter a command: ");
+        let mut command = String::new();
         io::stdin().read_line(&mut command).expect("Failed to read line");
-        let command = Some(command.trim());
-        //use match pattern to determine the command
+        let command = command.trim().to_string();
+        if command == "quit" {
+            break;
+        }
+        let mut split_command = command.split_whitespace();
+        let command = split_command.next().unwrap();
+
+        //pattern match add, list and delete employees
         match command {
-            Some("add_member") => {
-                println!("Enter the new name: ");
-                let mut name: String = String::new();
-                io::stdin().read_line(&mut name).expect( "Failed to read line");
-                let name: &str = name.trim();
-                println!("Enter a department: ");
-                let mut department: String = String::new();
-                io::stdin().read_line(&mut department).expect("Failed to read line");
-                let department: &str = department.trim();
-                //create a new employee and add it to the vector
-                let new_employee = Employee{
-                    name: name.to_string(),
-                    department: department.to_string(),
-                };
-                employees.push(new_employee);
-                //add the newest employee name, relative department to hashmap
-                full_house.insert(name, department);
-            },
-            Some("list_members") => {
-                println!("Enter a specific department: ");
-                let mut department = String::new();
-                io::stdin().read_line(&mut department).expect("Failed to read line");
-                let department = department.trim();
-                //use the hash map to get the employees in the department
-                let employees = full_house.get(department).unwrap();
-                println!("Employees in the department: {:?}", employees);
-            },
-            Some("delete_member") => {
-                println!("Who is leaving?");
-                let mut name = String::new();
-                io::stdin().read_line(&mut name).expect("Failed to read line");
-                let name = name.trim();
-                //use hash map to find the employee and delete them from the hash map
-                full_house.remove(name);
-            },
-            None => {
-                println!("Goodbye!");
-                break
-            },  
+            "add" => {
+                let name = split_command.next().unwrap();
+                let department = split_command.next().unwrap();
+                employees.insert(name.to_string(), department.to_string());
+                //print new employee
+                println!("{} added to {}", name, department);
+            }
+            "list" => {
+                let specific_department = split_command.next().unwrap();
+                let mut employees_in_department = Vec::new();
+                for (name, department) in employees.iter() {
+                    if department == specific_department {
+                        employees_in_department.push(name.to_string());
+                    }
+                }
+                employees_in_department.sort();
+                for name in employees_in_department {
+                    println!("{}", name);
+                }
+            }
+            "delete" => {
+                let name = split_command.next().unwrap();
+                employees.remove(name);
+            }
+            _ => {
+                println!("Invalid command");
+            }
         }
     }
 }
